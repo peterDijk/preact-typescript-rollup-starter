4c0e83f5907a2f5a45b56d190426e42a733f2fcd
Peter van Dijk <petervandijk@gmail.com>
2019-10-02 17:17:24 +0200

    dev server rollup-serve

--- rollup.config.js
+++ rollup.config.js
@@ -2,43 +2,38 @@ import replace from 'rollup-plugin-replace';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
-import uglify from 'rollup-plugin-uglify';
+import serve from 'rollup-plugin-serve';
+// import uglify from 'rollup-plugin-uglify';
 
 export default {
     input: './dist/es7/index.js',
     output: {
         file: `dist/app.bundle.js`,
-        format: 'esm', // what is iife
+        format: 'iife', // what is iife
         sourcemap: true,
-        // treeshake: true, // what why?
+        treeshake: true, // what why?
 
     },
     plugins: [
         replace({
             'process.env.NODE_ENV': JSON.stringify('production'),
           }),
-        //   babel({
-        //     exclude: 'node_modules/**' // only transpile our source code
-        //   }),
+          babel({
+            exclude: 'node_modules/**' // only transpile our source code
+          }),
         resolve(),
-        // resolve({
-        //     jsnext: true,
-        //     // module: false,
-        //     // browser: true,
-        //     main: true,
-        //     extensions: ['.js', '.jsx'],
-        //     // preferBuiltins: true,
-        //   }),
         commonjs(),
-        // uglify.uglify({
-        //     compress: {
-        //       screw_ie8: true,
-        //       warnings: false
-        //     },
-        //     output: {
-        //       comments: false
-        //     },
-        //     sourcemap: false,
-        //   }),
+        serve({
+            // Launch in browser (default: false)
+            open: true,
+           
+            // Page to navigate to when opening the browser.
+            // Will not do anything if open=false.
+            // Remember to start with a slash.
+            openPage: '/index.html',
+            host: 'localhost',
+            port: 8000,
+        })
+        // uglify ? 
     ],
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,4 @@
 import {h, render} from 'preact'
 import HelloWorld from './HelloWorld'
 
-render(<HelloWorld name='World' />, document.body)
-
-const foo = new Promise((resolve, reject) => {
-    const foo = {a:1, b:2, c:3}
-    setTimeout(() => resolve({...foo, a: 99}))
-})
-
-foo.then(fooDone => console.log(fooDone));
+render(<HelloWorld name='world' />, document.body)
