e4d5a8be324bb5bc7e6e75bb24013e5363553be1
Peter van Dijk <petervandijk@gmail.com>
2019-10-02 16:09:38 +0200

    working

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import {name} from './package.json'
+
+export default {
+    input: './dist/es7/app.js',  // See also package.json scripts.rollup
+    output: {
+        file: `dist/app.bundle.js`,
+        format: 'esm',
+        sourcemap: true,
+    },
+    plugins: [
+        resolve(),
+        commonjs(),
+    ],
+}
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import {h, render} from 'preact'
+import HelloWorld from './HelloWorld'
+
+render(<HelloWorld name='World' />, document.body)
+
+const foo = new Promise((resolve, reject) => {
+    const foo = {a:1, b:2, c:3}
+    setTimeout(() => resolve({...foo, a: 99}))
+})
+
+foo.then(fooDone => console.log(fooDone));
